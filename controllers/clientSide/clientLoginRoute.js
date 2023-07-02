b0c1522b995d818cad973685d4090b3396ae968c
Jean Xavier <xxajean@rakbank.ae>
2023-07-02 11:37:01 +0400

    feat: added client login controller

---
+++ controllers/clientSide/clientLoginRoute.js
@@ -0,0 +1,36 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { getRouteByKey } = require('../../routes/routes');
+const Client = require('./../../schemas/clientSchema');
+
+const clientLoginRoute = {
+  path: getRouteByKey('clientLoginIn'),
+  method: 'post',
+  handler: async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+      const user = await Client.findOne({ email }).exec();
+
+      if (!user) return res.status(401).json({ response: 'User not found' });
+
+      const { _id: id, password: passwordHash } = user;
+      const verifyPassword = await bcrypt.compare(password, passwordHash);
+
+      if (verifyPassword) {
+        jwt.sign({ id, password }, process.env.JWT_SECRET, { expiresIn: '5m' }, (error, token) => {
+          if (error) {
+            res.status(500).json(error);
+          }
+          res.status(200).json({ token });
+        });
+      } else {
+        res.status(401).json({ response: 'Unauthorized' });
+      }
+    } catch (error) {
+      res.sendStatus(500);
+    }
+  },
+};
+
+module.exports = { clientLoginRoute };
---
+++ controllers/clientSide/clientSignUpRoute.js
@@ -0,0 +1,44 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const Client = require('./../../schemas/clientSchema');
+const { getRouteByKey } = require('./../../routes/routes');
+
+const clientSignUpRoute = {
+  path: getRouteByKey('clientSignUp'),
+  method: 'post',
+  handler: async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+      await Client.findOne({ email });
+
+      const passwordHash = await bcrypt.hash(password, 10);
+      const newUser = await Client.create({ email, password: passwordHash });
+
+      const { _id, email: savedEmail } = newUser;
+      jwt.sign(
+        {
+          id: _id,
+          savedEmail,
+        },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: '10m',
+        },
+        (err, token) => {
+          if (err) {
+            return res.status(500).send(err);
+          }
+          res.status(200).json({ token });
+        }
+      );
+    } catch (error) {
+      if (error.constructor.name === 'MongoServerError' && error.code === 11000) {
+        return res.status(409).json({ response: 'User already exits' });
+      }
+      return res.status(500).json({ response: 'Internal server error' });
+    }
+  },
+};
+
+module.exports = { clientSignUpRoute };
