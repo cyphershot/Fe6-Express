95971b12e1f0dec0ba615c8ffacf53ff9346c2d8
ajmalameen <ajmalameenkm10@gmail.com>
2023-07-06 17:50:22 +0530

    nodeMailerFix

--- app.js
+++ app.js
@@ -3,14 +3,26 @@ const cors = require('cors');
 const routes = require('./routes/index');
 const { establishDbConnection } = require('./mongo');
 require('dotenv').config();
+const User = require("./schemas/userSchema");
+const router = require("./routes/router")
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 
+
+
 app.use(cors()); // Add this line to enable CORS
 
 app.use(express.json());
+
+app.use(router)
+
+app.get("/",cors(),(req,res)=>{
+
+})
+// client side
+const clientRoute = require("./clientside/clientSide")
 
 routes.forEach(route => {
   app[route.method](route.path, route.handler);
